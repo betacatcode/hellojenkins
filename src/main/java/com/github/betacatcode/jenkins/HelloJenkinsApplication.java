b434dff0ccccd8c1e0ce6fc903835313f8880338
wangxiao <1595990651@qq.com>
2021-02-22 11:58:18 +0800

    内容修改

--- src/main/java/com/github/betacatcode/jenkins/HelloJenkinsApplication.java
+++ src/main/java/com/github/betacatcode/jenkins/HelloJenkinsApplication.java
@@ -8,7 +8,7 @@ public class HelloJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloJenkinsApplication.class, args);
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins!!!");
     }
 
 }
